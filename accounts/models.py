d798fd169d4c24b944d1f4827f19d077adbc327d
parsa <paghsis@gmail.com>
2024-06-22 10:34:14 +0330

    remove & start from scratch

--- accounts/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-# Create your models here.
-
-class CustomeUser(AbstractUser):
-    age = models.PositiveIntegerField(null=True, blank=True)
-    
--- accounts/urls.py
+++
@@ -1,7 +0,0 @@
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('signup/', views.SignUpViews.as_view(), name='signup'),
-]
