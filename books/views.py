2c60203e00862b4d718d6cdd491c88dec14de4b3
pxsa <paghsis@gmail.com>
2024-07-03 00:44:32 +0330

    create book list view

--- books/views.py
+++ books/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+
+from .models import Book
+
 
 # Create your views here.
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = 'books'
+    template_name = 'books/book_list.html'
