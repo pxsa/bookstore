4040a1bcab6417352903dedd024bba792662deb3
pxsa <paghsis@gmail.com>
2024-07-03 11:47:59 +0330

    book create view

--- books/views.py
+++ books/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
+from django.urls import reverse_lazy
 
 from .models import Book
 
@@ -15,3 +16,10 @@ class BookDetailView(generic.DetailView):
     model = Book
     context_object_name = 'book'
     template_name = 'books/book_detail.html'
+
+
+class BookCreateView(generic.CreateView):
+    model = Book
+    template_name = 'books/book_create.html'
+    fields = ['title', 'author', 'description', 'price']
+    success_url = reverse_lazy('book_list')
