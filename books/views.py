f1d9711dae147c35c35b7af3b6b32ca10d63c8db
pxsa <paghsis@gmail.com>
2024-07-04 20:38:44 +0330

    showing comments for each book

--- books/views.py
+++ books/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.urls import reverse_lazy
 
@@ -16,6 +16,15 @@ class BookDetailView(generic.DetailView):
     model = Book
     context_object_name = 'book'
     template_name = 'books/book_detail.html'
+
+def book_detail_view(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    comments = book.comments.all()
+    context = {
+        'book': book,
+        'comments':comments,
+    }
+    return render(request, 'books/book_detail.html', context=context)
 
 
 class BookCreateView(generic.CreateView):
