6421ba2c05032b174d39ddb6d09cc64b4b7bb7d6
pxsa <paghsis@gmail.com>
2024-09-26 15:59:40 +0330

    login

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # installed
+    'widget_tweaks',
+
+    # custom
     'accounts.apps.AccountsConfig',
     'books.apps.BooksConfig',
 ]
