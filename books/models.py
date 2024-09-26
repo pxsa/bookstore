efc23d93f956ce3bc17e724c2ae9ce9d501951b7
pxsa <paghsis@gmail.com>
2024-09-26 06:57:56 +0330

    start app books

---
+++ books/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Books(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
